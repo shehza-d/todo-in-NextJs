f050d8d33c2b217206e9b7c2c7ee8a555929ebf2
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-12 20:49:13 +0500

    git push init

--- old-yt-code/app/page.tsx
+++ old-yt-code/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 import TodoList from "../components/TodoList";
 import AddNewTodo from "../components/addNewToDo";
 
